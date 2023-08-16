0e3637ea2a0c0ce00f6c9a627841fb681a9913a5
yashvi <you@example.com>
2023-08-16 16:24:51 +0530

    movies

--- Routes/movie-routes.js
+++ Routes/movie-routes.js
@@ -3,31 +3,30 @@ const multer = require("multer");
 
 const { movieImage, moviedetails } = require("../controller/movie-controller");
 const router = Router();
-router.get("/",(req,res)=>{
-    res.send("welcome to home")
-})
+router.get("/", (req, res) => {
+  res.send("welcome to home");
+});
+
 let storage = multer.diskStorage({
   destination: "images",
   filename: (req, file, cb) => {
     cb(null, Date.now() + file.originalname);
   },
 });
+
 const upload = multer({
   storage: storage,
 }).array("profile");
 
-router.post("/image", upload,movieImage);
+router.post("/image", upload, movieImage);
 
+router.get("/image", (req, res) => {
+  res.render("imguplode");
+});
 
-router.get("/image",(req,res)=> {
-    res.render("imguplode");
-  });
-
-
-router.get("/moviedetails",(req,res)=>{
-    console.log(req.body);
-    res.send("movie details added")
-    
-} )
+router.get("/moviedetails", (req, res) => {
+  console.log(req.body);
+  res.send("movie details added");
+});
 
 module.exports = router;
