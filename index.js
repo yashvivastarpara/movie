ce5a6592d230a0c7b713326ae5f7f565ff024dc9
yashvi <you@example.com>
2023-08-17 10:04:10 +0530

    add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connect = require("./config/db.js");
 const router = require("./Routes/movie-routes.js");
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
